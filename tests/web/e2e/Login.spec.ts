78ebbdbaf37b4f48478ab9efe3ad370673d400d3
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-09-09 06:59:57 -0300

    Adicionando beforeEach e verificando o perfil de usuário logado

--- tests/web/e2e/Login.spec.ts
+++ tests/web/e2e/Login.spec.ts
@@ -4,47 +4,48 @@ const users = require('../../fixtures/users.json');
 const MESSAGE_LOGIN_INVALID = 'Email ou senha incorretos.'
 const MESSAGE_LOGIN_SUCCESS = 'Login realizado com sucesso!'
 
-test("Should not login with wrong credentials", async ({ homePage, loginPage }) => {
+test.beforeEach(async ({ homePage }) => {
+  await homePage.clickOnRegister()
+})
+
+test("Should not login with wrong credentials", async ({ loginPage }) => {
   const user = users.wrong_credentials
 
-  await homePage.clickOnRegister()
   await loginPage.expectTitleRegistrationForm('Entre com sua conta')
   await loginPage.fillForm(user.email, user.password)
   await loginPage.expectAlertMessage(MESSAGE_LOGIN_INVALID)
 })
 
-test("Should not login with wrong email", async ({ homePage, loginPage }) => {
+test("Should not login with wrong email", async ({ loginPage }) => {
   const user = users.wrong_email
 
-  await homePage.clickOnRegister()
   await loginPage.expectTitleRegistrationForm('Entre com sua conta')
   await loginPage.fillForm(user.email, user.password)
   await loginPage.expectAlertMessage(MESSAGE_LOGIN_INVALID)
 })
 
-test("Should not login with wrong password", async ({ homePage, loginPage }) => {
+test("Should not login with wrong password", async ({ loginPage }) => {
   const user = users.wrong_password
 
-  await homePage.clickOnRegister()
   await loginPage.expectTitleRegistrationForm('Entre com sua conta')
   await loginPage.fillForm(user.email, user.password)
   await loginPage.expectAlertMessage(MESSAGE_LOGIN_INVALID)
 })
 
-test("Should login success with common user", async ({ homePage, loginPage }) => {
+test("Should login success with common user", async ({ loginPage, dashboardPage }) => {
   const user = users.common_user
 
-  await homePage.clickOnRegister()
   await loginPage.expectTitleRegistrationForm('Entre com sua conta')
   await loginPage.fillForm(user.email, user.password)
   await loginPage.expectAlertSuccessLogin(MESSAGE_LOGIN_SUCCESS)
+  await dashboardPage.userTypeLogged(user.logged)
 })
 
-test("Should login success with admin user", async ({ homePage, loginPage }) => {
+test("Should login success with admin user", async ({ loginPage, dashboardPage }) => {
   const user = users.admin_user
 
-  await homePage.clickOnRegister()
   await loginPage.expectTitleRegistrationForm('Entre com sua conta')
   await loginPage.fillForm(user.email, user.password)
   await loginPage.expectAlertSuccessLogin(MESSAGE_LOGIN_SUCCESS)
+  await dashboardPage.userTypeLogged(user.logged)
 })
