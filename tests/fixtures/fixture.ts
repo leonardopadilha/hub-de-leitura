490f0276168b7da06c370b4e66b524d59fc081eb
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-09-09 06:38:08 -0300

    inclusão da página de catálogo e do dashboard

--- tests/fixtures/fixture.ts
+++ tests/fixtures/fixture.ts
@@ -1,10 +1,14 @@
 import { test as base } from '@playwright/test';
 import HomePage from '../web/page-objects/Home';
 import LoginPage from '../web/page-objects/Login';
+import DashboardPage from '../web/page-objects/Dashboard';
+import CatalogPage from '../web/page-objects/Catalog';
 
 export const test = base.extend<{
   homePage: HomePage,
-  loginPage: LoginPage
+  loginPage: LoginPage,
+  dashboardPage: DashboardPage,
+  catalogPage: CatalogPage
 }>({
   homePage: async ({ page }, use) => {
     const homePage = new HomePage(page)
@@ -14,5 +18,14 @@ export const test = base.extend<{
   loginPage: async ({ page }, use) => {
     const loginPage = new LoginPage(page)
     await use(loginPage)
+  },
+  dashboardPage: async ({ page }, use) => {
+    const dashboardPage = new DashboardPage(page)
+    //await dashboardPage.visitar()
+    await use(dashboardPage)
+  },
+  catalogPage: async ({ page }, use) => {
+    const catalogPage = new CatalogPage(page)
+    await use(catalogPage)
   }
 })
