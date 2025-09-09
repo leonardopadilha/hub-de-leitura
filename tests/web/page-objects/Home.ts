34afbc1c4e42f332a480c237b483c78497901e1e
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-09-09 07:01:29 -0300

    Mapeando direcionamento para tela de catálogo a partir da home

--- tests/web/page-objects/Home.ts
+++ tests/web/page-objects/Home.ts
@@ -3,17 +3,28 @@ import { Locator, Page, expect } from "@playwright/test";
 export default class HomePage {
   private readonly page: Page
   private readonly btnRegister: Locator
+  private readonly btnBookCatalog: Locator
 
   constructor(page: Page) {
     this.page = page
     this.btnRegister = page.getByRole('link', { name: 'Criar Conta Grátis' });
+    this.btnBookCatalog = page.getByRole('button', { name: /Explorar Catálogo/i})
   }
 
   async visitar() {
     await this.page.goto('/')
+      // Expect a title "to contain" a substring.
+    await expect(this.page).toHaveURL(/localhost/);
+    
+    const pageTitle = await this.page.locator('.logo-section h1').textContent();
+    await expect(pageTitle).toEqual('Hub de Leitura');
   }
 
   async clickOnRegister() {
     await this.btnRegister.click()
+  }
+
+  async clickOnBookCatalog() {
+    await this.btnBookCatalog.click()
   }
 }
